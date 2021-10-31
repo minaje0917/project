0dde16ebaf1fa63974cb3773e861cfc3b010394a
선민재 <81280223+minaje0917@users.noreply.github.com>
2021-10-31 13:02:19 +0900

    황금사우나

---
+++ js/index.js
@@ -0,0 +1,14 @@
+document.querySelector('.버튼1').addEventListener('click', function(){
+    document.querySelector('.container').style.transform =
+    'translate(0vw)'
+});
+
+document.querySelector('.버튼2').addEventListener('click', function(){
+    document.querySelector('.container').style.transform =
+    'translate(-100vw)'
+});
+
+document.querySelector('.버튼3').addEventListener('click', function(){
+    document.querySelector('.container').style.transform =
+    'translate(-200vw)'
+});
